3fa5ffc80405a16e9e32a50094d46fbbf701f575
stephen fortune <steve.fortune@icecb.com>
2015-07-09 18:50:17 +0100

    Converted tabs to spaces

--- collection.js
+++ collection.js
@@ -31,103 +31,103 @@
  */
 (function() {
 
-	'use strict';
+  'use strict';
 
-	var Collection = function(query, loadNow) {
+  var Collection = function(query, loadNow) {
 
-		/**
-		 * @public
-		 * @deprecated We've found a way to inherit from the native
-		 * Array object. See here:
-		 * https://github.com/Urigo/angular-meteor/blob/master/modules/angular-meteor-collections.js#L14
-		 * @var Array
-		 */
-		this.arr = this;
+    /**
+     * @public
+     * @deprecated We've found a way to inherit from the native
+     * Array object. See here:
+     * https://github.com/Urigo/angular-meteor/blob/master/modules/angular-meteor-collections.js#L14
+     * @var Array
+     */
+    this.arr = this;
 
-		/**
-		 * @protected
-		 * @var Function(Number)
-		 */
-		this.query = query;
+    /**
+     * @protected
+     * @var Function(Number)
+     */
+    this.query = query;
 
-		/**
-		 * The state of the collection whilst loading.
-		 *
-		 * @protected
-		 * @var String: 'loading' | 'loaded' | 'failed'
-		 */
-		this.loadingState;
+    /**
+     * The state of the collection whilst loading.
+     *
+     * @protected
+     * @var String: 'loading' | 'loaded' | 'failed'
+     */
+    this.loadingState;
 
-		/**
-		 * Current promised used for loading more.
-		 *
-		 * @protected
-		 * @var Promise
-		 */
-		this.moreQ;
+    /**
+     * Current promised used for loading more.
+     *
+     * @protected
+     * @var Promise
+     */
+    this.moreQ;
 
-		if (typeof loadNow === 'undefined' || loadNow) {
-			this.refresh();
-		}
+    if (typeof loadNow === 'undefined' || loadNow) {
+      this.refresh();
+    }
 
-	};
+  };
 
-	Collection.prototype = [];
+  Collection.prototype = [];
 
-	/**
-	 * Loads more items into the collection by calling the query.
-	 *
-	 * @note  The obj returned by the Promise has the following
-	 *       key / vals:
-	 *      - items: Array, the new recently added items
-	 *       - oldLength: The previous length of the collection
-	 *       - newLength: The new length of the collection
-	 *       - firstSuccessfulQuery: A boolean indicating whether
-	 *         this was the first successful load of the
-	 *        collection.
-	 * @return   Promise
-	 */
-	Collection.prototype.more = function() {
+  /**
+   * Loads more items into the collection by calling the query.
+   *
+   * @note  The obj returned by the Promise has the following
+   *       key / vals:
+   *      - items: Array, the new recently added items
+   *       - oldLength: The previous length of the collection
+   *       - newLength: The new length of the collection
+   *       - firstSuccessfulQuery: A boolean indicating whether
+   *         this was the first successful load of the
+   *        collection.
+   * @return   Promise
+   */
+  Collection.prototype.more = function() {
 
-		var col = this;
-		if (col.moreQ) {
-			return col.moreQ;
-		}
+    var col = this;
+    if (col.moreQ) {
+      return col.moreQ;
+    }
 
-		var lenBef = col.length;
-		var stateBef = col.loadingState;
+    var lenBef = col.length;
+    var stateBef = col.loadingState;
 
-		col.moreQ = col.query(lenBef).then(function(items) {
-			Array.prototype.push.apply(col, items);
-			var lenAf = col.length;
-			col.loadingState = 'loaded';
-			return {
-				items: items,
-				oldLength: lenBef,
-				newLength: lenAf,
-				firstSuccessfulQuery: stateBef === 'loading' && lenAf > 0
-			};
-		}, function(err) {
-			col.loadingState = 'failed';
-			return err;
-		}).finally(function() {
-			col.moreQ = null;
-		});
+    col.moreQ = col.query(lenBef).then(function(items) {
+      Array.prototype.push.apply(col, items);
+      var lenAf = col.length;
+      col.loadingState = 'loaded';
+      return {
+        items: items,
+        oldLength: lenBef,
+        newLength: lenAf,
+        firstSuccessfulQuery: stateBef === 'loading' && lenAf > 0
+      };
+    }, function(err) {
+      col.loadingState = 'failed';
+      return err;
+    }).finally(function() {
+      col.moreQ = null;
+    });
 
-		return col.moreQ;
+    return col.moreQ;
 
-	};
+  };
 
 
-	/**
-	* Clears the collection and starts its loading again
-	*
-	* @return Promise
-	*/
-	Collection.prototype.refresh = function() {
-		this.length = 0;
-		this.loadingState = 'loading';
-		return this.more();
-	};
+  /**
+  * Clears the collection and starts its loading again
+  *
+  * @return Promise
+  */
+  Collection.prototype.refresh = function() {
+    this.length = 0;
+    this.loadingState = 'loading';
+    return this.more();
+  };
 
 })();
